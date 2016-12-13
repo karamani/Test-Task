3c84228a69350ebc7ce2394dd852fe0387cd3b24
Voedoo <voedoo@gmail.com>
2016-12-13 07:45:50 +0300

    Update createtable.sql

--- createtable.sql
+++ createtable.sql
@@ -1,1 +1,3 @@
-CREATE TABLE pricelist (distributorId INT NOT NULL, art INT NOT NULL, count INT, price INT, CONSTRAINT id PRIMARY KEY (distributorId, art))
+CREATE DATABASE IF NOT EXISTS sql7149110;
+USE sql7149110;
+CREATE TABLE pricelist (distributorId INT NOT NULL, art INT NOT NULL, count INT, price INT, CONSTRAINT id PRIMARY KEY (distributorId, art));
